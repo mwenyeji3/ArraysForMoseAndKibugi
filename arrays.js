37e6fa7f0227b613e236686d8e258de89b8bc7e2
mwenyeji3 <31016177+mwenyeji3@users.noreply.github.com>
2017-09-19 22:29:09 -0400

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,82 @@
+(function() {
+  'use strict';
+
+  window.arrays = {};
+
+  
+
+  arrays.practiseArray = function(){
+var practiseArray =[];
+return practiseArray;
+	};
+
+ // don't touch this line
+var practiseArray = arrays.practiseArray();
+
+//TODOS
+//Complete the functions to make the tests in the specrunner file pass
+arrays.addString = function(){
+
+arrays [0] = 'Mose and Kibugi';
+
+return arrays ;
+	};
+
+arrays.reassignValue = function(){
+arrays [0]='it is really fun learning javascritpt' ;
+
+return arrays;  
+};
+
+arrays.checkLength = function(){
+arrays.checkLength =practiseArray;
+
+return arrays;
+};
+
+
+arrays.addFriends = function(){
+arrays [0]='moses, Kibugi';
+
+	arrays.push = 'friend';
+
+	return arrays; 
+};
+
+
+arrays.addThreeValues = function () {
+
+	arrays.of(5);
+
+	return arrays;
+   
+};
+
+arrays.removeLastItem = function () {
+
+  arrays.pop[5];
+
+  return arrays ;
+};
+
+arrays.removeFirstItem = function () {
+
+	arrays.shift[1]
+
+	return arrays;
+ 
+};
+
+
+arrays.addFirstItem = function(){
+
+	arrays.unshift('Beauty');
+
+	return arrays ;
+  
+};
+
+
+
+
+}());
