a429d4af4a4774599285573ea69fd29e89f7472f
mwenyeji3 <31016177+mwenyeji3@users.noreply.github.com>
2017-09-28 16:44:46 -0400

    Add files via upload

---
+++ Assignment2.js
@@ -0,0 +1,37 @@
+//PROBLEM
+//Given an array and an integer, rotate the array so that the item
+//at any position will move to a new position n times further.
+// Meaning each value should move n times
+
+var rotateArrayLeft = function (array, n) {
+  //TODO
+}
+
+//Test
+//var testArray = [1,2,3,4,5,6,7,8,9];
+
+var rotateArrayLeft = function (array, n) {
+  
+var count = [];
+
+for ( var i = 0 ; i < array.length ; i++){
+
+	var newIndex = ((i + n) % array.length);
+	
+
+		count.push(array[newIndex])
+}
+
+return count.filter((parse)=>parse % 2 !== 0).filter((mose)=> mose > 6 )
+
+
+};
+  
+console.log(rotateArrayLeft([1,2,3,4,5,6,7,8,9],6));
+
+
+
+
+
+
+ // Should log [ 7, 8, 9, 1, 2, 3, 4, 5, 6 ]
